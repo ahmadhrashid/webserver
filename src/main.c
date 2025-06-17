17317feebf35c355bedced3e3a88e9553a035f47
Ahmad Rashid <ahmadhrashid05@gmail.com>
2025-06-17 02:37:30 -0400

    implemented graceful shutdown using signal handling

--- src/main.c
+++ src/main.c
@@ -3,10 +3,22 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include "queue.h"
+#include <signal.h>
+
+volatile sig_atomic_t stop = 0;
+
+void handle_sigint(int signum) {
+    stop = 1;
+}
 
 void threadpool_start(); // from threadpool.c
 
 int main() {
+    signal(SIGINT, handle_sigint);
+    if (signal(SIGPIPE, SIG_IGN) == SIG_ERR) {
+        perror("Failed to ignore SIGPIPE");
+        exit(EXIT_FAILURE);
+    }
     int server_fd, client_fd;
     struct sockaddr_in addr;
     socklen_t addrlen = sizeof(addr);
@@ -23,10 +35,21 @@ int main() {
 
     printf("Threaded server listening on port 8080...\n");
 
-    while (1) {
+    while (!stop) {
         client_fd = accept(server_fd, (struct sockaddr *)&addr, &addrlen);
+        if (client_fd < 0) {
+            if (stop) break; // Exit loop if server is stopping
+            perror("Failed to accept connection");
+            continue;
+        }
         queue_push(client_fd);
+        printf("Accepted connection from %s:%d\n", inet_ntoa(addr.sin_addr), ntohs(addr.sin_port));
+
     }
+    printf("Shutting down server...\n");
+    fflush(stdout);
+    printf("Server shutdown complete.\n");
+     
 
     close(server_fd);
     queue_destroy();
