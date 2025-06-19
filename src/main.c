d1b16e21a52af7a9f1e8ed459eb347d89c333ca0
Ahmad Rashid <ahmadhrashid05@gmail.com>
2025-06-18 23:54:43 -0400

    Fixed logs to correctly display handled GET requests

--- src/main.c
+++ src/main.c
@@ -11,11 +11,13 @@
 
 volatile sig_atomic_t stop = 0;
 
-void handle_sigint(int signum) {
+void handle_sigint(int signum)
+{
     (void)signum; // Unused parameter
     stop = 1;
 }
-void install_sigint_handler() {
+void install_sigint_handler()
+{
     struct sigaction sa;
     memset(&sa, 0, sizeof(sa));
     sa.sa_handler = handle_sigint;
@@ -23,11 +25,13 @@ void install_sigint_handler() {
     sigaction(SIGINT, &sa, NULL);
 }
 void threadpool_start(); // from threadpool.c
-void threadpool_stop(); 
+void threadpool_stop();
 
-int main() {
+int main()
+{
     install_sigint_handler();
-    if (signal(SIGPIPE, SIG_IGN) == SIG_ERR) {
+    if (signal(SIGPIPE, SIG_IGN) == SIG_ERR)
+    {
         perror("Failed to ignore SIGPIPE");
         exit(EXIT_FAILURE);
     }
@@ -35,38 +39,47 @@ int main() {
     threadpool_start();
 
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd < 0) { perror("socket"); exit(EXIT_FAILURE); }
+    if (server_fd < 0)
+    {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
 
     int opt = 1;
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
+    {
         perror("setsockopt SO_REUSEADDR");
         exit(EXIT_FAILURE);
     }
 
-    struct sockaddr_in addr = { .sin_family = AF_INET,
-                                .sin_addr.s_addr = INADDR_ANY,
-                                .sin_port = htons(8080) };
-    if (bind(server_fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
-        perror("bind"); exit(EXIT_FAILURE);
+    struct sockaddr_in addr = {.sin_family = AF_INET,
+                               .sin_addr.s_addr = INADDR_ANY,
+                               .sin_port = htons(8080)};
+    if (bind(server_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    {
+        perror("bind");
+        exit(EXIT_FAILURE);
     }
-    if (listen(server_fd, 10) < 0) {
-        perror("listen"); exit(EXIT_FAILURE);
+    if (listen(server_fd, 10) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
     }
 
     printf("Threaded server listening on port 8080...\n");
 
     socklen_t addrlen = sizeof(addr);
-    while (!stop) {
-        int client_fd = accept(server_fd, (struct sockaddr*)&addr, &addrlen);
-        if (client_fd < 0) {
-            if (errno == EINTR && stop) break;  // interrupted by SIGINT
+    while (!stop)
+    {
+        int client_fd = accept(server_fd, (struct sockaddr *)&addr, &addrlen);
+        if (client_fd < 0)
+        {
+            if (errno == EINTR && stop)
+                break; // interrupted by SIGINT
             perror("accept");
             continue;
         }
         queue_push(client_fd);
-        printf("Accepted connection from %s:%d\n",
-               inet_ntoa(addr.sin_addr),
-               ntohs(addr.sin_port));
     }
 
     printf("Shutting down server...\n");
@@ -77,4 +90,3 @@ int main() {
     printf("Server shutdown complete.\n");
     return 0;
 }
-
--- src/threadpool.c
+++ src/threadpool.c
@@ -4,126 +4,154 @@
 #include <string.h>
 #include <fcntl.h>
 #include <sys/stat.h>
-#include <sys/socket.h>    // for SO_RCVTIMEO
-#include <sys/time.h>      // for struct timeval
+#include <sys/socket.h> // for SO_RCVTIMEO
+#include <sys/time.h>   // for struct timeval
 #include "queue.h"
 #include "globals.h"
 
 #define THREAD_COUNT 4
 #define WWW_DIR "./www"
 
-int shutdown_flag = 0;  // Global shutdown flag
+int shutdown_flag = 0; // Global shutdown flag
 
 static pthread_t threads[THREAD_COUNT];
 
 // Worker thread function
-static void* worker(void* arg) {
+static void *worker(void *arg)
+{
     (void)arg;
-    while (1) {
-        if (shutdown_flag) {
+    while (1)
+    {
+        if (shutdown_flag)
+        {
             printf("[worker %ld] popping queue:\n", pthread_self());
-           
         }
         int client_fd = queue_pop();
-        if (client_fd == -1) break;  // shutdown
-        struct timeval tv = { .tv_sec = 1, .tv_usec = 0 };  // 1 second timeout
+        if (client_fd == -1)
+            break;                                       // shutdown
+        struct timeval tv = {.tv_sec = 1, .tv_usec = 0}; // 1 second timeout
         if (setsockopt(client_fd, SOL_SOCKET, SO_RCVTIMEO,
-                    &tv, sizeof(tv)) < 0) {
+                       &tv, sizeof(tv)) < 0)
+        {
             perror("setsockopt SO_RCVTIMEO");
         }
         // Read request
         char buf[1024];
-        if (shutdown_flag) {
+        if (shutdown_flag)
+        {
             printf("[worker %ld] reading client_fd:\n", pthread_self());
-           
         }
-        int n = read(client_fd, buf, sizeof(buf)-1);
-        if (shutdown_flag) {
+        int n = read(client_fd, buf, sizeof(buf) - 1);
+        if (shutdown_flag)
+        {
             printf("[worker %ld] read %d bytes. client_fd: %d\n", pthread_self(), n, client_fd);
-           
         }
-        if (n > 0) {
+        if (n > 0)
+        {
             buf[n] = '\0';
             // Parse path
             char method[8], path[256];
-            if (shutdown_flag) {
-            printf("[worker %ld] sscanf:\n", pthread_self());
-           
+            if (shutdown_flag)
+            {
+                printf("[worker %ld] sscanf:\n", pthread_self());
             }
             sscanf(buf, "%s %s", method, path);
-            if (strcmp(path, "/") == 0) strcpy(path, "/index.html");
+            printf("Request: %s\n", path);
+            if (strcmp(path, "/") == 0)
+                strcpy(path, "/index.html");
+
+            if (strcmp(method, "GET") != 0 ||
+                strcmp(path, "/favicon.ico") == 0)
+            {
+                close(client_fd);
+                continue;
+            }
+
+            printf("Handled GET %s from worker %lu\n",
+                   path, pthread_self());
 
             // Build full path
             char fullpath[512];
             snprintf(fullpath, sizeof(fullpath), "%s%s", WWW_DIR, path);
-            if (shutdown_flag) {
-            printf("[worker %ld] opening fd:\n", pthread_self());
-           
+            if (shutdown_flag)
+            {
+                printf("[worker %ld] opening fd:\n", pthread_self());
             }
             // Try to open
             int fd = open(fullpath, O_RDONLY);
-            if (fd < 0) {
-                if (shutdown_flag) {
-            printf("[worker %ld] writing not found response:\n", pthread_self());
-           
-            }
+            if (fd < 0)
+            {
+                if (shutdown_flag)
+                {
+                    printf("[worker %ld] writing not found response:\n", pthread_self());
+                }
                 const char *not_found =
                     "HTTP/1.0 404 Not Found\r\n"
                     "Content-Type: text/html\r\n\r\n"
                     "<h1>404 Not Found</h1>";
                 write(client_fd, not_found, strlen(not_found));
-            } else {
-                if (shutdown_flag) {
-            printf("[worker %ld] writing response:\n", pthread_self());
-           
             }
+            else
+            {
+                if (shutdown_flag)
+                {
+                    printf("[worker %ld] writing response:\n", pthread_self());
+                }
                 const char *header =
                     "HTTP/1.0 200 OK\r\n"
                     "Content-Type: text/html\r\n\r\n";
                 write(client_fd, header, strlen(header));
                 char filebuf[1024];
                 int r;
-                if (shutdown_flag) {
-            printf("[worker %ld] some random read write stuff:\n", pthread_self());
-           
-            }
-                while ((r = read(fd, filebuf, sizeof(filebuf))) > 0){
-                    if (shutdown_flag) printf("[worker %ld] writing filebuf:\n", pthread_self());
+                if (shutdown_flag)
+                {
+                    printf("[worker %ld] some random read write stuff:\n", pthread_self());
+                }
+                while ((r = read(fd, filebuf, sizeof(filebuf))) > 0)
+                {
+                    if (shutdown_flag)
+                        printf("[worker %ld] writing filebuf:\n", pthread_self());
                     write(client_fd, filebuf, r);
                 }
-                    
-                if (shutdown_flag) printf("[worker %ld] closing fd:\n", pthread_self());
+
+                if (shutdown_flag)
+                    printf("[worker %ld] closing fd:\n", pthread_self());
                 close(fd);
-                if (shutdown_flag) printf("[worker %ld] closed fd:\n", pthread_self());
+                if (shutdown_flag)
+                    printf("[worker %ld] closed fd:\n", pthread_self());
             }
         }
-        if (shutdown_flag) {
+        if (shutdown_flag)
+        {
             printf("[worker %ld] closing client_fd:\n", pthread_self());
-           
         }
         close(client_fd);
-        if (shutdown_flag) {
+        if (shutdown_flag)
+        {
             printf("[worker %ld] closed client_fd:\n", pthread_self());
-           
         }
     }
     return NULL;
 }
 
 // Start THREAD_COUNT workers
-void threadpool_start() {
-    for (int i = 0; i < THREAD_COUNT; ++i) {
+void threadpool_start()
+{
+    for (int i = 0; i < THREAD_COUNT; ++i)
+    {
         pthread_create(&threads[i], NULL, worker, NULL);
     }
 }
 
 // Gracefully stop: wake all workers & join them
-void threadpool_stop() {
-    shutdown_flag = 1;  
+void threadpool_stop()
+{
+    shutdown_flag = 1;
     printf("[pool] stopping, about to shutdown queue\n");
     queue_shutdown();
     printf("[pool] queue_shutdown done, now joining threads\n");
-    for (int i = 0; i < THREAD_COUNT; ++i) {
+    for (int i = 0; i < THREAD_COUNT; ++i)
+    {
         pthread_join(threads[i], NULL);
     }
 }
